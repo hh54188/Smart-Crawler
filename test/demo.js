8c2e29216cb3ad077dd1c612255b4647a91bc9c8
unknown <hh54188@9AC08D9B9A65960.internal.baidu.com>
2014-12-18 20:06:31 +0800

    update

--- test/demo.js
+++ test/demo.js
@@ -1,9 +1,8 @@
 var Crawler = require("../refactor");
 
 var c = new Crawler({
-	url: "http://example.com",
-	selector: "p",
+	url: "http://www.douban.com/group/beijingzufang/discussion?start=0",
 	callback: function (err, $) {
-		console.log("$--->", $);
+		console.log($("#content table tr"));
 	}
 });
